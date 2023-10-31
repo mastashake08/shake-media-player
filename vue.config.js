957400450334ae24996434be6ad9118bf61374d1
Hacker God <jyrone.parker@gmail.com>
2023-10-31 18:59:23 -0400

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/shake-media-player/'
+    : '/'
   pwa: {
     id: '/',
     themeColor: "#5853FF",
