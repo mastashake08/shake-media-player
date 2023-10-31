78f4f2c7c1426c8a0f05c538d400874f438295dc
Jyrone Parker <jyrone.parker@atlasied.com>
2023-10-31 12:01:28 -0400

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Shake Media Logo" src="./assets/logo.png" height="250" width="250">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,31 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    themeColor: "#130f40",
+    msTileColor: "#130f40",
+    name: "My app",
+    manifestOptions: {
+      icons: [
+        {
+          src: "./img/icons/maskable_icon_x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "./img/icons/maskable_icon_x512.png",
+          sizes: "512x512",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "./img/icons/maskable_icon.png",
+          sizes: "1024x1024",
+          type: "image/png",
+          purpose: "maskable"
+        }
+      ],
+    },
+  },
 })
